1e2f9fe95bacbdde250c9e4aa3170de740d2a46b
Rachel Warner <RMWarner96@gmail.com>
2025-06-17 17:09:23 -0400

    Changed style, updated projects

--- page-scripts.js
+++ page-scripts.js
@@ -1,5 +1,10 @@
 
-function openPDF() {
+function openpwtPDF() {
             const pdfPath = 'pwt.pdf';
+            window.open(pdfPath, '_blank'); // Open in a new window or tab
+}
+
+function opengardenPDF() {
+            const pdfPath = 'My_Garden_App_Documentation.pdf';
             window.open(pdfPath, '_blank'); // Open in a new window or tab
 }
